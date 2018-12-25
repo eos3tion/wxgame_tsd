e1fb2be36280cefbd3564bada65e7f211545533a
程方 <chengfang@junyougame.com>
2018-12-25 11:28:03 +0800

    新增电量获取的定义 完善`Platform`常量

--- wxgame.d.ts
+++ wxgame.d.ts
@@ -17,121 +17,113 @@ declare namespace wx {
 
     const enum Platform {
         Android = "android",
-        DevTools = "devtools"
+        DevTools = "devtools",
+        IOS = "ios"
     }
 
 
     interface SystemInfo {
         /**
          * 手机品牌  
-         * 适用版本：>= 1.5.0
+         * @version >= 1.5.0
          */
         brand: string;
 
 
         /**
          * 手机型号  
-         * 适用版本：
+         * 
          */
         model: string;
 
 
         /**
          * 设备像素比  
-         * 适用版本：
+         * 
          */
         pixelRatio: number;
 
 
         /**
          * 屏幕宽度  
-         * 适用版本：>= 1.1.0
+         * @version >= 1.1.0
          */
         screenWidth: number;
 
 
         /**
          * 屏幕高度  
-         * 适用版本：>= 1.1.0
+         * @version >= 1.1.0
          */
         screenHeight: number;
 
 
         /**
          * 可使用窗口宽度  
-         * 适用版本：
+         * 
          */
         windowWidth: number;
 
 
         /**
          * 可使用窗口高度  
-         * 适用版本：
+         * 
          */
         windowHeight: number;
 
+        /**
+         * 状态栏的高度
+         * @version >= 1.9.0
+         */
+        statusBarHeight: number;
 
         /**
          * 微信设置的语言  
-         * 适用版本：
+         * 
          */
         language: string;
 
 
         /**
          * 微信版本号  
-         * 适用版本：
+         * 
          */
         version: string;
 
 
         /**
          * 操作系统版本  
-         * 适用版本：
+         * 
          */
         system: string;
 
 
         /**
          * 客户端平台  
-         * 适用版本：
+         * 
          */
         platform: Platform;
 
 
         /**
          * 用户字体大小设置。以“我-设置-通用-字体大小”中的设置为准，单位 px。  
-         * 适用版本：>= 1.5.0
+         * @version >= 1.5.0
          */
         fontSizeSetting: number;
 
 
         /**
          * 客户端基础库版本  
-         * 适用版本：>= 1.1.0
+         * @version >= 1.1.0
          */
         SDKVersion: string;
 
 
         /**
          * 性能等级，-2 或 0：该设备无法运行小游戏，-1：性能未知，>=1 设备性能值，该值越高，设备性能越好(目前设备最高不到50)  
-         * 适用版本：>= 1.8.0
+         * @version >= 1.8.0
          */
         benchmarkLevel: number;
-
-
-        /**
-         * 电量，范围 1 - 100  
-         * 适用版本：>= 1.9.0
-         */
-        battery: number;
-
-
-        /**
-         * wifi 信号强度，范围 0 - 4  
-         * 适用版本：>= 1.9.0
-         */
-        wifiSignal: number;
     }
 
     interface GetSystemInfoCallback extends Callback {
@@ -145,16 +137,16 @@ declare namespace wx {
 
     /**
      * 获取系统信息
-     *
-     * @
+     * https://developers.weixin.qq.com/minigame/dev/api/wx.getSystemInfo.html
+     * 
      * @param {GetSystemInfoCallback} callback
      */
     function getSystemInfo(callback: GetSystemInfoCallback)
 
     /**
      * wx.getSystemInfo 的同步版本
-     *
-     * @
+     * https://developers.weixin.qq.com/minigame/dev/api/wx.getSystemInfoSync.html
+     * 
      * @returns {SystemInfo}
      */
     function getSystemInfoSync(): SystemInfo;
@@ -1267,6 +1259,35 @@ declare namespace wx {
      */
     function openCustomerServiceConversation(param?: OpenCustomerServiceConversationParam);
 
+    //--------------------------电量---------------------------------
+
+    interface GetBattleryInfoSuccessRtn {
+        /**
+         * 设备电量，范围 1 - 100
+         */
+        level: string;
+        /**
+         * 	是否正在充电中
+         */
+        isCharging: boolean;
+    }
+
+    interface GetBattleryInfoParam extends Callback {
+        success(ret: GetBattleryInfoSuccessRtn)
+    }
+
+    /**
+     * 获取设备电量。 在 iOS 上不可用。  
+     * https://developers.weixin.qq.com/minigame/dev/api/wx.getBatteryInfo.html
+     * 实际测试，此API在ios上不可用
+     */
+    function getBatteryInfo(param: GetBattleryInfoParam);
+
+    /**
+     * 获取设备电量的同步版本  
+     * https://developers.weixin.qq.com/minigame/dev/api/wx.getBatteryInfoSync.html
+     */
+    function getBatteryInfoSync(): GetBattleryInfoSuccessRtn;
 
     //--------------------------剪贴板-------------------------------
     interface SetClipboardDataParam extends Callback {
